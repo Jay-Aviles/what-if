7cd484d4b4526583905f2b13a04124f8ac27b535
Jay-Aviles <105664972+Jay-Aviles@users.noreply.github.com>
2022-05-23 19:53:27 -0400

    Create weekday.js

---
+++ weekday.js
@@ -0,0 +1,30 @@
+
+const prompt = require("prompt-sync")();
+
+const num = Number(prompt("Enter a number for  a day of the week:"));
+
+if (num === 1) {
+    console.log("Sunday",);
+} else if (num === 2) {
+    console.log("Monday:",);
+}
+else if (num === 3) {
+    console.log ("Tuesday:",);
+}
+else if (num === 4) {
+    console.log("Wednesday:",);
+
+}
+else if (num === 5) {
+    console.log("Thursday:",);
+}
+else if (num === 6) {
+    console.log("Friday:",);
+}
+else if (num === 7) {
+    console.log("Saturday",);
+}
+else {
+    console.log("That is a error");
+}
+
